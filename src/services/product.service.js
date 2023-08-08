84dbc1b6163e94b3cfe14bbc8fe89fb94ca35dc7
Saurav <sauravkumar00108@gmail.com>
2023-08-08 22:41:58 +0530

    last commit

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const addProduct=(data)=>axios.post('http://localhost:5000/product/',data);
-export const getProducts= ()=>axios.get(`http://localhost:5000/product/`);
-export const deleteProduct= (id)=>axios.delete(`http://localhost:5000/product/${id}`);
-export const updateMyProduct= (data,id)=>axios.put(`http://localhost:5000/product/${id}`,data);
+export const addProduct=(data)=>axios.post('http://crud-api-omega.vercel.app/product/',data);
+export const getProducts= ()=>axios.get(`http://crud-api-omega.vercel.app/product/`);
+export const deleteProduct= (id)=>axios.delete(`http://crud-api-omega.vercel.app/product/${id}`);
+export const updateMyProduct= (data,id)=>axios.put(`http://crud-api-omega.vercel.app/product/${id}`,data);
